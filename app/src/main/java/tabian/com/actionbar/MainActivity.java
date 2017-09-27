036dd9d4786b406689ee123c55e30920c09f90c9
Charlon Palacay <charlon@gmail.com>
2017-09-27 10:31:36 -0700

    Initial commit of actionbar app conversion for scout android.

---
+++ app/src/main/java/tabian/com/actionbar/MainActivity.java
@@ -0,0 +1,161 @@
+package tabian.com.actionbar;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.basecamp.turbolinks.TurbolinksSession;
+import com.basecamp.turbolinks.TurbolinksAdapter;
+import com.basecamp.turbolinks.TurbolinksView;
+
+public class MainActivity extends AppCompatActivity implements TurbolinksAdapter {
+    private static final String TAG = "MainActivity";
+
+
+    // basic turbolinks setup
+    private static final String BASE_URL = "https://scout-test.s.uw.edu/h/seattle/";
+    private static final String INTENT_URL = "intentUrl";
+
+    private String location;
+    private TurbolinksView turbolinksMainView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the custom TurbolinksView object in your layout
+        turbolinksMainView = (TurbolinksView) findViewById(R.id.turbolinks_main_view);
+
+        // For this demo app, we force debug logging on. You will only want to do
+        // this for debug builds of your app (it is off by default)
+        TurbolinksSession.getDefault(this).setDebugLoggingEnabled(true);
+
+        // For this example we set a default location, unless one is passed in through an intent
+        location = getIntent().getStringExtra(INTENT_URL) != null ? getIntent().getStringExtra(INTENT_URL) : BASE_URL;
+
+        // Execute the visit
+        TurbolinksSession.getDefault(this)
+                .activity(this)
+                .adapter(this)
+                .view(turbolinksMainView)
+                .visit(location);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.ic_arrow:
+
+                        break;
+
+                    case R.id.ic_android:
+                        Intent intent1 = new Intent(MainActivity.this, ActivityFood.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.ic_books:
+                        Intent intent2 = new Intent(MainActivity.this, ActivityStudy.class);
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.ic_center_focus:
+                        Intent intent3 = new Intent(MainActivity.this, ActivityTech.class);
+                        startActivity(intent3);
+                        break;
+
+                }
+
+
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        // Since the webView is shared between activities, we need to tell Turbolinks
+        // to load the location from the previous activity upon restarting
+        TurbolinksSession.getDefault(this)
+                .activity(this)
+                .adapter(this)
+                .restoreWithCachedSnapshot(true)
+                .view(turbolinksMainView)
+                .visit(location);
+    }
+
+    // -----------------------------------------------------------------------
+    // TurbolinksAdapter interface
+    // -----------------------------------------------------------------------
+
+    @Override
+    public void onPageFinished() {
+
+    }
+
+    @Override
+    public void onReceivedError(int errorCode) {
+        handleError(errorCode);
+    }
+
+    @Override
+    public void pageInvalidated() {
+
+    }
+
+    @Override
+    public void requestFailedWithStatusCode(int statusCode) {
+        handleError(statusCode);
+    }
+
+    @Override
+    public void visitCompleted() {
+
+    }
+
+    // The starting point for any href clicked inside a Turbolinks enabled site. In a simple case
+    // you can just open another activity, or in more complex cases, this would be a good spot for
+    // routing logic to take you to the right place within your app.
+    @Override
+    public void visitProposedToLocationWithAction(String location, String action) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(INTENT_URL, location);
+
+        this.startActivity(intent);
+    }
+
+    // -----------------------------------------------------------------------
+    // Private
+    // -----------------------------------------------------------------------
+
+    // Simply forwards to an error page, but you could alternatively show your own native screen
+    // or do whatever other kind of error handling you want.
+    private void handleError(int code) {
+        if (code == 404) {
+            TurbolinksSession.getDefault(this)
+                    .activity(this)
+                    .adapter(this)
+                    .restoreWithCachedSnapshot(false)
+                    .view(turbolinksMainView)
+                    .visit(BASE_URL + "/error");
+        }
+    }
+
+}
